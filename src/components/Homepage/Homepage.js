4b1aecacdfe6d8cc7062fa0b63a861525f5898fc
Tasja <tasjalaurie@outlook.com>
2018-06-20 16:25:32 +1000

    Added banner to customer products page

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
+import AdBanner from '../AdBanner/AdBanner';
+
 class Homepage extends Component {
 
     render() {
         return ( 
             <div>
-                <p>Hello I am the homepage</p>           
+                <p>Hello I am the homepage</p>  
+                <AdBanner />         
             </div> 
         );
     }
