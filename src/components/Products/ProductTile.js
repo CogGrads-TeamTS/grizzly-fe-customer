8b99120de3e7c47f2d7c14844fb6432211da7e10
Blake Howe <blakehowe@hotmail.com>
2018-06-21 22:00:26 +1000

    setup reusable component for product tiles

--- src/components/Products/ProductTile.js
+++ src/components/Products/ProductTile.js
@@ -1,28 +1,19 @@
 import React from 'react';
-import { CardGroup, Card, CardTitle, CardText, CardImg, CardImgOverlay, Fade, Button, CardSubtitle } from 'reactstrap';
+import { CardGroup, Card, CardTitle, CardText, CardImg, CardImgOverlay, Fade, Button, CardSubtitle, CardBody, CardLink, Badge  } from 'reactstrap';
 import './ProductTile.css';
 
 const ProductTile = (props) => {
-
-  
   return (
     <div className="tile">
         <Fade>
-            <Card inverse>
-            <CardImg style={{"width": "100%"}} src="http://assets.kraftfoods.com/recipe_images/opendeploy/%20138280-49fdab4f7bf207b3cc31f72186c86b0a642f0802_642x428.jpg" alt="Card image cap" />
+            <Card >
+            <CardImg style={{"width": "100%"}} src={props.imageUrl} alt="Card image cap" />
             
-            {!props.main && 
-                <CardImgOverlay>
-                <CardTitle>{props.product.name}</CardTitle>
-                <CardText>
-                    ${props.product.price}
-                </CardText>
-                <CardText className="d-xl-none">
-                    <small>{props.product.description}</small>
-                </CardText>
-                
-                </CardImgOverlay>
-            }
+            <CardBody>
+                <CardTitle>{props.text}</CardTitle>
+                {props.badge && <Badge color={props.badgeColor}>{props.badge}</Badge>}
+            </CardBody>
+            
             </Card>
         </Fade>
     </div>
