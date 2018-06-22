c7417858bc67d48db9a18577eb7c0ef0e85fe52e
Blake Howe <blakehowe@hotmail.com>
2018-06-22 11:14:14 +1000

    use products from props

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { Container, Row, Col } from 'reactstrap';
+import { Container } from 'reactstrap';
 
 import { productsFetchData } from '../../actions/productActions'
 
@@ -10,7 +10,7 @@ import ProductTiles from '../Products/ProductTiles';
 import AdBanner from '../AdBanner/AdBanner';
 
 class Homepage extends Component {
-    main = {text: 'Smartphones',imageUrl: 'https://i.nextmedia.com.au/Utils/ImageResizer.ashx?n=https%3A%2F%2Fi.nextmedia.com.au%2Fgalleries%2F20180412105252_iphone-lineup400.jpg&h=450&w=800&c=0&s=0', badge: 'Category', badgeColor: 'info'}
+    main = {imageUrl: 'https://cdn.bloomnation.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/2/0/20161202060048_file_5841b6d088276.png'}
     productData = [
         {id: 1, name: 'Cake', imageUrl: 'http://assets.kraftfoods.com/recipe_images/opendeploy/%20138280-49fdab4f7bf207b3cc31f72186c86b0a642f0802_642x428.jpg'},
         {id: 1, name: 'Cake', imageUrl: 'http://assets.kraftfoods.com/recipe_images/opendeploy/%20138280-49fdab4f7bf207b3cc31f72186c86b0a642f0802_642x428.jpg'},
@@ -43,9 +43,10 @@ class Homepage extends Component {
     render() {
         return ( 
             <div>
-                <AdBanner />
+                <p>Hello I am the homepage</p>  
+                <AdBanner />                    
                 <Container fluid>
-                    {this.props.products ? <ProductTiles main={this.main} products={this.props.products} /> : <p>No Products</p>}
+                    {this.props.products ? <ProductTiles products={this.props.products} /> : <p>No Products</p>}
                 </Container>
             </div>
         );
