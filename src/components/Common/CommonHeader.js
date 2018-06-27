247dc9354adf5aeaa0c8cb20ecd0197a59f67c32
Tasja <tasjalaurie@outlook.com>
2018-06-27 16:13:00 +1000

    Linked Grizzly Store home button and favicon

--- src/components/Common/CommonHeader.js
+++ src/components/Common/CommonHeader.js
@@ -9,6 +9,7 @@ import {
     NavItem,
     NavLink,
     Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 import grizzlogo from '../../assets/griz-logo.png';
 
@@ -28,18 +29,14 @@ class Header extends Component {
           isOpen: !this.state.isOpen
         });
     }
-
-    goTo(route) {
-        this.props.history.replace(`/${route}`)
-      }
     
-      login() {
+    login() {
         this.props.auth.login();
-      }
+    }
     
-      logout() {
+    logout() {
         this.props.auth.logout();
-      }
+    }
 
     render() {
         const { isAuthenticated } = this.props.auth; 
@@ -47,7 +44,7 @@ class Header extends Component {
         return (
             <div>
                 <Navbar light expand="md">
-                    <NavbarBrand><img className="griz-logo" src={grizzlogo} onClick={this.goTo.bind(this, '')}/></NavbarBrand>
+                    <NavbarBrand><Link to="/"><img className="griz-logo" src={grizzlogo} /></Link></NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
