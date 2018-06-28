50b29372a848aeb9985200936f7f1c90a5f35216
Blake Howe <blakehowe@hotmail.com>
2018-06-28 12:58:42 +1000

    adjusted badge positions

--- src/components/Products/ProductTile.js
+++ src/components/Products/ProductTile.js
@@ -14,17 +14,16 @@ const ProductTile = (props) => {
         <Fade>
             <Card className={props.cardClass}>
             <CardImg style={{"width": "100%"}} src={`${imageUrl}${props.imageUrl}`} alt="Card image cap" />
-            
             <CardBody>
-                <CardTitle>{props.text}</CardTitle>
-                {props.badge && <Badge color={props.badgeColor}>{props.badge}</Badge>}
+                {/* <CardTitle>{props.text}</CardTitle> */}
+                {props.badge && <Badge color={props.badgeColor} className="badge-badge">{props.badge}</Badge>}
                 {
-                    props.product &&
-                    <Badge color={"success"} id={tooltip_price}>${props.product.price}</Badge>
+                props.product &&
+                    <Badge color={"success"} className="badge-price" id={tooltip_price}>${props.product.price}</Badge>
                 }
                 {
                     props.product &&
-                    <Badge color={"secondary"} id={tooltip_brand}>{props.product.brand}</Badge>
+                    <Badge color={"secondary"} className="badge-brand" id={tooltip_brand}>{props.product.brand}</Badge>
                 }
                 {
                     props.product && 
