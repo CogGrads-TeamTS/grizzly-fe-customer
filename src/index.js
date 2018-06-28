36a0ccbae5ab2ea37f2f0a5a60b76e451e6f810f
Tasja <tasjalaurie@outlook.com>
2018-06-28 17:04:01 +1000

    WIP: Login/Signup changed to use Auth0 Lock

--- src/index.js
+++ src/index.js
@@ -7,19 +7,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Auth from './Auth/Auth'
 import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 const store = configureStore();
-const auth = new Auth();
 
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App auth={auth}/>
+            <App />
         </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
--- src/routes.js
+++ src/routes.js
@@ -5,25 +5,30 @@ import Homepage from './components/Homepage/Homepage';
 import ProductSingle from './components/Products/ProductSingle';
 import history from './history';
 import Callback from './Callback/Callback';
+import Login from './Auth/Login';
+import Logout from './Auth/Logout';
 
 
 const Routes = (props) => {
-    const auth = props.auth;
-    const handleAuthentication = ({location}) => {
-        if (/access_token|id_token|error/.test(location.hash)) {
-          auth.handleAuthentication();
-        }
-    }
+//     const auth = props.auth;
+//     const handleAuthentication = ({location}) => {
+//         if (/access_token|id_token|error/.test(location.hash)) {
+//           auth.handleAuthentication();
+//         }
+//     }
+
+          // <Route path="/callback" render={(props) => {
+                //     handleAuthentication(props);
+                //     return <Callback {...props} auth={props.auth} /> 
+          //  }}/> 
     
     return (
         <div>
             <Switch history={history}>
                 <Route exact path="/" component={Homepage} />
                  <Route path="/product/:id" component={ProductSingle} />
-                <Route path="/callback" render={(props) => {
-                    handleAuthentication(props);
-                    return <Callback {...props} auth={props.auth} /> 
-                  }}/> 
+                 <Route path="/login" component={Login} />
+                 <Route path="/logout" component={Logout} />
             </Switch>
         </div>
     )
