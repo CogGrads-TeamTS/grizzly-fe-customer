7738539f42aa6fc513e1678f804ae2b65de44c71
Codevski <1435321+codevski@users.noreply.github.com>
2018-07-04 16:03:24 +1000

    Added Header Cart

--- src/components/Common/CommonHeader.js
+++ src/components/Common/CommonHeader.js
@@ -19,14 +19,34 @@ import grizzlogo from '../../assets/griz-logo.png';
 import { fetchUserByID } from '../../actions/userActions';
 
 
+const CartCount = ({ cart }) => {
+    if (cart && cart.items && cart.items.length > 0) {
+      let itemsCount = cart.items.reduce((a, b) => a + b.quantity, 0);
+      return <span className="cart-count">{itemsCount}</span>;
+    } else {
+      return null;
+    }
+  }
+  
+  const CartIcon = ({ cartIsActive }) => {
+      console
+    if(cartIsActive){
+      return <img src="/assets/images/close.svg" className="icon"   style={{ minWidth: 24, padding: 4 }}/>
+    } else {
+      return <img src="/assets/images/shopping-bag.svg" className="icon"   style={{ minWidth: 24, padding: 4}}/>
+    }
+  }
+
 class Header extends Component {
     constructor(props) {
         super(props);
     
         this.state = {
-          isOpen: false
+          isOpen: false,
+          cartIsActive: false
         };
         this.toggle = this.toggle.bind(this);
+        this.cartToggle = this.cartToggle.bind(this);
     }
 
     componentDidMount(){
@@ -39,7 +59,19 @@ class Header extends Component {
         });
     }
 
+    cartToggle = () => {
+        console.log("TEST");
+        console.log(!this.state.cartIsActive);
+        this.setState({
+          cartIsActive: !this.state.cartIsActive,
+          mobileMenuIsActive: false
+        });
+        document.body.classList.toggle('noscroll');
+      }
+    
+
     render() {
+        const { cart } = this.props;
         return (
                 <Navbar light expand="md">
                     <NavbarBrand><Link to="/"><img className="griz-logo" src={grizzlogo} /></Link></NavbarBrand>
@@ -53,7 +85,14 @@ class Header extends Component {
                                             <NavLink className="welcome-name" disabled href="#">Welcome, {this.props.user !== undefined && this.props.user.name}!</NavLink>
                                         )
                                     }
-                                </NavItem>            
+                                </NavItem>
+                                <span className="cart-button" onClick={this.cartToggle}>
+                                    <CartIcon cartIsActive={this.state.cartIsActive} />
+                                    <CartCount cart={cart} />
+                                </span>
+                                <div className={this.state.cartIsActive ? 'mini-cart-open' : ''}>
+                                    <div className="mini-cart"><p>Your cart is empty</p></div>
+                                </div>
                                 <NavItem>
                                     <NavLink href="#">                             
                                         {
