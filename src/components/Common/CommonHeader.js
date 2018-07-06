3781e0147cfc4006dd9a986f98d9bd6beaa92437
Auphonix <matchstick_dan@hotmail.com>
2018-07-06 18:55:34 +1000

    Restarting navbar layout

--- src/components/Common/CommonHeader.js
+++ src/components/Common/CommonHeader.js
@@ -35,6 +35,15 @@ class Header extends Component {
         this.toggle = this.toggle.bind(this);
         this.cartToggle = this.cartToggle.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
+
+        String.prototype.trunc =
+            function (n, useWordBoundary) {
+                if (this.length <= n) { return this; }
+                var subString = this.substr(0, n - 1);
+                return (useWordBoundary
+                    ? subString.substr(0, subString.lastIndexOf(' '))
+                    : subString) + " ...";
+            };
     }
 
     componentDidMount() {
@@ -42,11 +51,11 @@ class Header extends Component {
         this.props.fetchCart(false);
     }
 
-    callbackLogoUpdate(active){
-        if(active)
-        this.setState({grizzlyClass: " logo-active"})
-        else 
-        this.setState({grizzlyClass: ""})
+    callbackLogoUpdate(active) {
+        if (active)
+            this.setState({ grizzlyClass: " logo-active" })
+        else
+            this.setState({ grizzlyClass: "" })
     }
 
     toggle() {
@@ -81,75 +90,79 @@ class Header extends Component {
     render() {
         const { cart } = this.props;
         return (
-            <Navbar light expand="md">
+            <div>
                 <div style={{ width: "100%" }}>
                     <NavbarBrand><Link to="/"><img className={"griz-logo" + this.state.grizzlyClass} src={grizzlogo} /></Link></NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                 </div>
-                <div>
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <GlobalSearch classname="global-search-user" logoCallback={this.callbackLogoUpdate.bind(this)} rounded="user-search-rounded" placeholder="Search" />
-                            <NavItem>
-                                {
-                                    isAuthenticated() && (
-                                        <NavLink className="welcome-name" disabled href="#">Welcome, {this.props.user !== undefined && this.props.user.name}!</NavLink>
-                                    )
-                                }
-                            </NavItem>
-                            {console.log(cart)}
-                            <CartIndicator cart={cart} onClick={this.cartToggle} cartIsActive={this.state.cartIsActive} />
-                            <div className={this.state.cartIsActive ? 'mini-cart-open' : ''}>
-                                <Cart cart={cart} deleteCartItem={this.deleteItem} />
-                            </div>
-                            <NavItem>
-                                <NavLink href="#">
-                                    {
-                                        !isAuthenticated() && (
-                                            <Link to="/login">
-                                                <Button
-                                                    id="btn-rounded"
-                                                    className="login-button"
-                                                >
-                                                    Log In
-                                                </Button>
-                                            </Link>
-                                        )
-                                    }
-                                    {
-                                        isAuthenticated() && (
-                                            <Link to="/logout">
-                                                <Button
-                                                    id="btn-rounded"
-                                                    className="signup-button"
-                                                >
-                                                    Log Out
-                                                </Button>
-                                            </Link>
-                                        )
-                                    }
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="#">
-                                    {
-                                        !isAuthenticated() && (
-                                            <Link to="/login">
-                                                <Button
-                                                    className="signup-button"
-                                                    id="btn-rounded"
-                                                >
-                                                    Signup
-                                                </Button>
-                                            </Link>
-                                        )
-                                    }
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </div>
-            </Navbar>
+
+            </div>
+            // <Navbar light expand="md">
+            //     
+            //     <div>
+            //         <Collapse isOpen={this.state.isOpen} navbar>
+            //             <Nav className="ml-auto" navbar>
+            //                 <GlobalSearch classname="global-search-user" logoCallback={this.callbackLogoUpdate.bind(this)} rounded="user-search-rounded" placeholder="Search" />
+            //                 <NavItem className="user-name-disp">
+            //                     {
+            //                         isAuthenticated() && (
+            //                             <NavLink className="welcome-name" disabled href="#">Welcome, {this.props.user !== undefined && this.props.user.name.trunc(10, true)}!</NavLink>
+            //                         )
+            //                     }
+            //                 </NavItem>
+            //                 {console.log(cart)}
+            //                 <CartIndicator cart={cart} onClick={this.cartToggle} cartIsActive={this.state.cartIsActive} />
+            //                 <div className={this.state.cartIsActive ? 'mini-cart-open' : ''}>
+            //                     <Cart cart={cart} deleteCartItem={this.deleteItem} />
+            //                 </div>
+            //                 <NavItem>
+            //                     <NavLink href="#">
+            //                         {
+            //                             !isAuthenticated() && (
+            //                                 <Link to="/login">
+            //                                     <Button
+            //                                         id="btn-rounded"
+            //                                         className="login-button"
+            //                                     >
+            //                                         Log In
+            //                                     </Button>
+            //                                 </Link>
+            //                             )
+            //                         }
+            //                         {
+            //                             isAuthenticated() && (
+            //                                 <Link to="/logout">
+            //                                     <Button
+            //                                         id="btn-rounded"
+            //                                         className="signup-button"
+            //                                     >
+            //                                         Log Out
+            //                                     </Button>
+            //                                 </Link>
+            //                             )
+            //                         }
+            //                     </NavLink>
+            //                 </NavItem>
+            //                 <NavItem>
+            //                     <NavLink href="#">
+            //                         {
+            //                             !isAuthenticated() && (
+            //                                 <Link to="/login">
+            //                                     <Button
+            //                                         className="signup-button"
+            //                                         id="btn-rounded"
+            //                                     >
+            //                                         Signup
+            //                                     </Button>
+            //                                 </Link>
+            //                             )
+            //                         }
+            //                     </NavLink>
+            //                 </NavItem>
+            //             </Nav>
+            //         </Collapse>
+            //     </div>
+            // </Navbar>
         )
     }
 }
