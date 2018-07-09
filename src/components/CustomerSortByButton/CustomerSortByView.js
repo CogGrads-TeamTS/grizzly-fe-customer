628abd454ca4e3477c86bef44137924f9e2072ce
Anju Thomas <anju.thomas2@cognizant.com>
2018-07-09 14:46:11 +1000

    -wip- preloader for shop by view page, preloader works, but grid appears after loading

--- src/components/CustomerSortByButton/CustomerSortByView.js
+++ src/components/CustomerSortByButton/CustomerSortByView.js
@@ -1,13 +1,12 @@
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Container,Row,Col,Button } from 'reactstrap';
 import {productsFetchData} from '../../actions/productActions'
 import ProductTiles from '../Products/ProductTiles';
-
+import ImagesLoaded from 'react-images-loaded';
 
 class CustomerSortByView extends Component {
-    constructor(props) { 
+    constructor(props) {
         super(props);
         this.search="";
         this.page = 0;
@@ -16,26 +15,67 @@ class CustomerSortByView extends Component {
         (props.match.params.id)?this.category=props.match.params.id:this.category="";
         (props.match.params.name)?this.brand=props.match.params.name:this.brand="";
         (props.match.params.rating)?this.rating=props.match.params.rating:this.rating="";
-        
+
+        this.state = {
+            loading: true
+        };
+        //this.loading = true;
     }
 
-    componentDidMount(){ 
+
+    componentDidMount(){
         // Keep here it may fix a problem later on - Dan
-        // this.props.fetchData(this.search, this.page, this.size, this.sort,this.category,this.brand,this.rating);
+        this.props.fetchData(this.search, this.page, this.size, this.sort,this.category,this.brand,this.rating);
+        this.setState({ loaded: "" ,
+        loading:false});
     }
+    handleOnAlways = instance => { };
 
-    render() { 
+    handleOnProgress = (instance, image) => { this.loading = true; };
+
+    handleOnFail = instance => { };
+
+    handleDone = instance => { this.loading = false; this.setState({ loaded: "complete-loaded" }); };
+    render() { console.log(this.props.products);
+
+        const isLoading= (!this.props.products || this.state.loading ) ?
+            (
+                <div className="loading-container-full-pre">
+                    <div className="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                </div>
+            ) : (
+                <div className="container-fluid product-container">
+                    <div className={"loading-container-full loaded" + (this.state && !this.state.loading ? this.state.loaded : "")}>
+                        <div className="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                    </div>
+                    <Container fluid>
+                        <Row>
+                            <Col md="2" sm="4" xs="12">
+                                <Button outline id="btn-rounded" onClick={this.returnToHome} className="m-t-10 btn-block" color="info">Back</Button>
+                            </Col>
+                        </Row>
+                        <ImagesLoaded
+                            elementType={'ul'} // defaults to 'div'
+                            className={'your-container-class'} // defaults to 'images-loaded-container'
+                            onAlways={this.handleOnAlways}
+                            onProgress={this.handleOnProgress}
+                            onFail={this.handleOnFail}
+                            done={this.handleDone}
+                            background=".image" // true or child selector
+                        >
+                            <div>
+                                <ProductTiles  products={this.props.products} />
+                            </div>
+                        </ImagesLoaded>
+                    </Container>
+                </div>
+            );
 
         return (
-            <Container fluid>
-                <Row>
-                    <Col md="2" sm="4" xs="12">
-                        <Button outline id="btn-rounded" onClick={this.returnToHome} className="m-t-10 btn-block" color="info">Back</Button>
-                    </Col>
-                </Row>
-                {this.props.loading ? <p>Loading....</p> : <p></p>}
-                {this.props.products && !this.props.loading ? <ProductTiles  products={this.props.products} /> : <p></p>}
-            </Container>
+            <div>
+                {isLoading}
+            </div>
+
         );
     }
     returnToHome = () => {
@@ -53,7 +93,7 @@ const mapStateToProps = (state) => {
     };
 };
 
-const mapDispatchToProps = (dispatch) => { 
+const mapDispatchToProps = (dispatch) => {
     return {
         fetchData: (search, page, size, sort, catId, brand,rating)=> dispatch(productsFetchData(search, page, size, sort, catId, brand,rating))
     };
