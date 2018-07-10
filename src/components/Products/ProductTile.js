45417288044adad97812c0f48e5ae59bf82ecf1d
Auphonix <matchstick_dan@hotmail.com>
2018-07-10 19:10:40 +1000

    resolved merge issues

--- src/components/Products/ProductTile.js
+++ src/components/Products/ProductTile.js
@@ -35,21 +35,6 @@ const ProductTile = (props) => {
                     {(props.product.discount > 0 ? <div className="discount">SAVE {props.product.discount}%</div> : null)}
                 </div>
                 <div className="star-cont">
-<<<<<<< HEAD
-                    <StarRatings
-                        rating={props.product.rating}
-                        numberOfStars={5}
-                        name='rating'
-                        starDimension="20px"
-                        starSpacing="2px"
-                        starRatedColor='rgb(108, 116, 217)'
-                        isSelectable={false}
-                    /></div>
-            </Link>
-            <Button color="primary" className="add-btn" onClick={addToCart}>ADD TO CART</Button>
-        </div>
-    );
-=======
                 <StarRatings 
                                     rating={props.product.rating}
                                     numberOfStars={5}
@@ -63,7 +48,6 @@ const ProductTile = (props) => {
                 <Button color="primary" className="add-btn btn-primary-griz" onClick={addToCart}>ADD TO CART</Button>
             </div>
   );
->>>>>>> development
 };
 
 export default ProductTile;
