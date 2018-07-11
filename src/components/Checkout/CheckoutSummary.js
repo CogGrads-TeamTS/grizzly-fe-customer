88fe30ed1cd5a4c354339f1a6a290d57402475ab
Blake Howe <blakehowe@hotmail.com>
2018-07-11 14:16:14 +1000

    redirect checkout

--- src/components/Checkout/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Card, CardTitle, CardImg, CardFooter, Fade, CardBody, Badge, UncontrolledTooltip, Row, Col  } from 'reactstrap';
 import _ from 'lodash';
+import { withRouter } from 'react-router-dom';
 import CartItem from '../Common/cart/cart';
 import QtyInput from '../Common/cart/QtyInput'
 
@@ -11,6 +12,8 @@ import config from '../../config';
 
 import {getFormValues, getFormSyncErrors} from 'redux-form';
 
+import SweetAlert from 'sweetalert2-react';
+
 const imageUrl = 'http://ts.ausgrads.academy/images/';
 
 const CLIENT = {
@@ -19,6 +22,20 @@ const CLIENT = {
 const ENV = 'sandbox';
 
 class CheckoutSummary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { showComplete: false };
+    }
+
+    completed(order) {
+        console.log(order);
+        this.setState({ showComplete: true })
+    }
+
+    closeCompleted() {
+        this.setState({ showComplete: false })
+        this.props.history.replace("/orders");
+    }
 
     componentWillMount() {
         // close the cart 
@@ -53,6 +70,16 @@ class CheckoutSummary extends React.Component {
                 </Row>
             );
             return (
+                <div>
+                <SweetAlert
+                    show={this.state.showComplete}
+                    title="Checkout Complete"
+                    text="Payment was processed successfully."
+                    type="success"
+                    onConfirm={() => {
+                        this.closeCompleted();
+                    }}
+                />
                 <Card style={{padding:"20px",margin: "25px"}}>
                     <CardTitle><h1 className="display-4">Order Summary</h1></CardTitle>
                     <CardBody>
@@ -93,11 +120,13 @@ class CheckoutSummary extends React.Component {
                                 commit={true}
                                 currency={'AUD'}
                                 total={this.props.cart.totalPrice}
+                                complete={this.completed.bind(this)}
                                 />
                             }
                         </Row>
                     </CardBody>
                 </Card>
+                </div>
             )
         } 
         return (
@@ -128,4 +157,4 @@ const mapDispatchToProps = (dispatch) => {
 
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(CheckoutSummary);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CheckoutSummary));
